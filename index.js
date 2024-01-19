ed38eb94a91bb450d9e6ce437aa1c4b924f6eedd
Tyler Clark <tylerc111@gmail.com>
2024-01-19 15:50:36 -0500

    updated inquirer to 8.2.4 and added a description to the package.json file

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your name?",
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
