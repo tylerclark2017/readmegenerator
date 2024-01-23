67815150db53174d3473bd2fe04d53ed32c72404
Tyler Clark <tylerc111@gmail.com>
2024-01-23 17:10:07 -0500

    added parameters for renderLicenseLink and renderLicenseSection functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,12 +15,30 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  
+  if (license === 'MIT License') {
+    return 'https://opensource.org/licenses/MIT/'
+  } else if (license === 'GNU General Public License v3') {
+    return 'https://gnu.org/licenses/gpl-3.0';
+  } else if (license === 'Apache License 2.0') {
+    return 'https://www.apache.org/licenses/LICENSE-2.0';
+  } else {
+    return '';
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license ==='MIT License') {
+    return 'This project is licensed under the MIT License. Check the license link for more details.';
+  } else if (license === 'GNU General Public License v3') {
+    return `This project is licensed under the GNU General Public License v3. Check the license link for more details.`;
+  } else if (license === 'Apache License 2.0') {
+    return 'This project is licensed under the Apache License 2.0. Check the license link for more details.';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
