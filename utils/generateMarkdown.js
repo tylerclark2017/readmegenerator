3c470f7978c6375b8f8cd5893d054c5cdf85fb4c
Tyler Clark <tylerc111@gmail.com>
2024-01-23 11:51:03 -0500

    added parameters for renderLicenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT License") {
+    return '![License: MIT]';
+  } else if (license ==='GNU General Public License v3') {
+    return '![License: GPL v3]';
+  } else if (license === 'Apache License 2.0') {
+    return '![License: Apache License 2.0]';
+    } else {
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
