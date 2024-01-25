ef11ccc3a9b2fbd9f40c146579e81ed8533f7ba8
Tyler Clark <tylerc111@gmail.com>
2024-01-25 14:38:06 -0500

    added in more functionality to hopefully closer align with acceptance criteria.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -26,10 +26,18 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license ==='None') {
-    return '';
-  } else {
-    return 'License';
+  if (license ==='MIT License') {
+    return `## License
+    This project is licensed under the MIT License.`;
+  } else if (license ==='GNU General Public License v3') {
+    return `## License
+    This project is licensed under the GNU General Public License v3.0`
+  } else if (license === 'Apache License 2.0') {
+    return `## License
+    This project is licensed under Apache License Version 2.0`;
+  }
+  else {
+    return `## License`
   }
 }
 
@@ -62,6 +70,8 @@ ${data.testing}
 ${renderLicenseSection(data.license)}
 
 ## Questions
+If you have any questions, please contact me at ${data.email}. You can find more of my work at [${data.githubUsername}](https://github.com/${data.githubUsername}).
+
 ### User Email
 ${data.email}
 
